1bd9b373931b1d736b51cb4bc432c9af5a57b304
Artem Ryzhikov <artemryzhikov@gmail.com>
2024-08-05 11:00:46 +0300

    Encoder's benchmark added

--- modules/encoder.py
+++ modules/encoder.py
@@ -9,7 +9,7 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.PReLU(inplace=True)
         self.with_abg = with_abg
         if with_abg:
             self.abg = AttentionBoostingGate()
@@ -42,18 +42,24 @@ class EfficientResNetEncoder(nn.Module):
 
 # Example usage
 if __name__ == "__main__":
-    encoder = EfficientResNetEncoder()
-    input_tensor = torch.randn(1, 3, 1024, 768)  # Example input tensor
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    encoder = EfficientResNetEncoder().to(device)
+    encoder.eval()
+    input_tensor = torch.randn(1, 3, 1024, 768).to(device)  # Example input tensor
     import time
 
-    for _ in range(100): # warm up
+    n_warmup_iters = 1
+    n_eval_iters = 1
+
+    for _ in range(n_warmup_iters): # warm up
         activations, output = encoder(input_tensor)
     print(f'Device: {output.device}')
 
     
     start_ts = time.time()
-    encoder(input_tensor)
-    print(f'Encoder inference (ms): {(time.time() - start_ts)*1000:.4f}') # 60 ms on CPU (!!!)
+    for _ in range(n_eval_iters):
+        encoder(input_tensor)
+    print(f'Encoder inference (ms): {(time.time() - start_ts)*1000/n_eval_iters:.4f}') # 60 ms on CPU (!!!)
     for act in activations:
         print(act.shape)
     print(output.shape)    
